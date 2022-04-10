ae730c45e8660164e30ab64317865efccf1dcf26
Peter Lappin <lappin-pj@ulster.ac.uk>
2022-04-10 15:09:30 +0100

    Added initial version of the crypto list

--- src/components/trading/TokenList.js
+++ src/components/trading/TokenList.js
@@ -3,25 +3,76 @@ import { Container } from "react-bootstrap";
 import LoadingOverlay from 'react-loading-overlay';
 import { marked } from 'marked';
 import { Navigation } from '../navigation/Navigation';
-import { fetchTokenList } from '../../utils/hiveEngine';
+import { fetchHiveEngineData } from '../../utils/hiveEngine';
 import tokenIconDefault from './tokenIconDefault.png';
-
+import  DataTable from 'react-data-table-component';
+ 
 export function TokenList() {
-    const [tokens, setTokens] = useState([{_id: '1', issuer: 'hive', symbol: 'HIVE', metadata: '{"url":"https://hive-engine.com","icon":"https://s3.amazonaws.com/steem-engine/images/icon_steem-engine_gradient.svg","desc":"BEE is the native token for the Hive Engine platform"}'}]);
+    const [tokens, setTokens] = useState([{_id: '1', issuer: 'hive', name: 'Hive Engine Token', symbol: 'HIVE', metadata: '{"url":"https://hive-engine.com","icon":"https://s3.amazonaws.com/steem-engine/images/icon_steem-engine_gradient.svg","desc":"BEE is the native token for the Hive Engine platform"}'}]);
+    const [metrics, setMetrics] = useState([{_id: '1', name:'Hive Engine Token', symbol: 'HIVE', volume: 0.00, lastPrice: 0.00, lowestAsk: 0.00, highestBid: 0.00, lastDayPrice: 0.00, priceChangeHive: 0.00, priceChangePercent: '0.00%'}])
+    const [tokensWithMetrics, setTokensWithMetrics] = useState([{_id: '1', issuer: 'hive', name: 'Hive Engine Token', symbol: 'HIVE', metadata: '{"url":"https://hive-engine.com","icon":"https://s3.amazonaws.com/steem-engine/images/icon_steem-engine_gradient.svg","desc":"BEE is the native token for the Hive Engine platform"}', volume: 0.00, lastPrice: 0.00, lowestAsk: 0.00, highestBid: 0.00, lastDayPrice: 0.00, priceChangeHive: 0.00, priceChangePercent: '0.00%'}])
     const [loading, setLoading] = useState(false);
 
     async function getTokens() {
-        const tokensResponse = await fetchTokenList();
+        const tokensResponse = await fetchHiveEngineData('tokens', 'tokens');
         console.log(tokensResponse);
         setTokens(tokensResponse);
-        setLoading(false)
+        return tokensResponse;
     }
 
+    async function getMetrics() {
+        const metricsResponse = await fetchHiveEngineData('market', 'metrics');
+        console.log(metricsResponse);
+        setMetrics(metricsResponse);
+        setLoading(false)
+        return metricsResponse;
+    }
     
+    async function mergeArrayObjects(){
+        
+        const tokens = await fetchHiveEngineData('tokens', 'tokens');
+        const metrics = await fetchHiveEngineData('market', 'metrics');
+        let merged = [];
+
+        for(let i=0; i<tokens.length; i++) {
+          merged.push({
+           ...tokens[i], 
+           ...(metrics.find((itmInner) => itmInner.symbol === tokens[i].symbol))}
+          );
+        }
+        console.log(merged);
+        setTokensWithMetrics(merged);
+      }
+
     useEffect(() => {
-        setLoading(true)
-        getTokens();
+        mergeArrayObjects();
     }, [])
+
+    const columns = [
+        {
+            sortable: true,
+            name: 'Token',
+            selector: row => row.symbol,
+        },
+        {
+            sortable: true,
+            name: 'Name',
+            selector: row => row.name,
+        },
+        {
+            sortable: true,
+            name: 'Volume',
+            selector: row => row.volume ? row.volume : 0.00,
+        },
+        {
+            sortable: true,
+            name: '24h % Change',
+            selector: row => row.priceChangePercent ? (parseFloat(row.priceChangePercent)) +'%': '0.00%',
+            style: row => ({
+                color: row ? 'gray' : 'red'
+            }),
+        },
+    ];
 
     return(
         <div className="App">
@@ -38,25 +89,9 @@ export function TokenList() {
                 <h1 style={{marginLeft: '2%',textAlign: 'left', color: 'white'}}>All Tokens</h1>
                 <br></br>
                 {
-                    tokens && tokens.map((token, index) => {
-                        return (
-                        <div style={{display: 'block', textAlign: 'left', marginRight: '3%', marginLeft: '3%', marginBottom: '2.5%'}}> 
-                            <div style={{display: 'inline', minWidth: '50px', width: '50px'}}>
-                                <img onError={({currentTarget}) => { currentTarget.src = tokenIconDefault}} style={{width: '50px', height: '50px', display: 'inline-block'}} referrerPolicy='no-referrer' src={JSON.parse(token.metadata).icon ? JSON.parse(token.metadata).icon : tokenIconDefault}></img>
-                                
+                    tokens && <div style={{borderRadius:'0px'}}>
+                                <DataTable  theme='dark' noHeader={true} columns={columns} data={tokensWithMetrics} pagination paginationPerPage={20} />
                             </div>
-                                <p style={{display: 'inline-block', color: 'white', marginLeft: '3%'}}>{token.symbol}</p>
-                                <hr
-                                    style={{
-                                        color: 'gray',
-                                        backgroundColor: 'gray',
-                                        height: 1
-                                    }}
-                                />
-                        </div>
-
-                        )
-                    })
                 }
                 <br></br>
             </Container>
--- src/utils/hiveEngine.js
+++ src/utils/hiveEngine.js
@@ -3,7 +3,6 @@ import SSC from 'sscjs';
 const ssc = new SSC('https://api.hive-engine.com/rpc');
 
 
-export async function fetchTokenList() {
-   return ssc.find('tokens', 'tokens', { }, 1000, 0, [], null);
+export async function fetchHiveEngineData(contract, table) {
+   return ssc.find(contract, table, { }, 1000, 0, [], null);
 }
-
